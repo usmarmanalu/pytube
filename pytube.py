6a04094025374f428022d60d680611d2eafbf599
usmarmanalu <usmarmnl99@gmail.com>
2022-01-05 18:34:49 +0700

    tambah repo pytube

---
+++ pytube.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+
+
+link = input("masukkan url youtube : ")
+yt = YouTube(link)
+videos = yt.streams.all()
+
+video = list(enumerate(videos))
+for i in video:
+    print(i)
+    
+    
+print("enter the desired option to dwonload format : ")
+dnoption = int(input("enter the option : "))
+dnoption = videos[dnoption]
+dnoption.dwonload()
+
+print("dwonload sukses")
